bd7e7990c2dfa9b962ecb36aa98a31250f7974f4
Jesin762 <jesinkj10@gmail.com>
2026-05-05 12:20:15 +0530

    backend deploy setup

--- journal/journal/settings.py
+++ journal/journal/settings.py
@@ -8,10 +8,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-REPLACE-THIS-IN-PRODUCTION'
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
-
+ALLOWED_HOSTS = ['*']
 
 # ----------------------------
 # INSTALLED APPS
